8830f7230eb99c1720ba74373880afbe22e28ffa
ClaudioBz <83524039+ClaudioBz@users.noreply.github.com>
2021-06-03 17:17:32 -0300

    Update CacheMatrix.R
    This commit contains my functions to Assignment2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## It's concerning that the content of the videos in this course don't match the requirements in the programming assignments
 
-## Write a short comment describing this function
+## This function will set values for the vector and the mean, and then get those values
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+        inv<-NULL
+        set<-function(y){
+                x<<-y
+                inv<<-NULL
+        }
+        get<-function() {x}
+        setInverse<-function(inverse) {inv<<-inverse}
+        getInverse<-function() {inv}
+        list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
+## The function below calculates the mean of the vector "inv". The rule "if("is.null(inv))" checks if the mean have been already calculated.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve<-function(x,...){
+        inv<-x$getInverse()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        mat<-x$get()
+        inv<-solve(mat,...)
+        x$setInverse(inv)
+        inv
+}
         ## Return a matrix that is the inverse of 'x'
 }
