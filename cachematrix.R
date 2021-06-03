71c357398e9cc0f05f0e53a12a517cd706ae3922
ClaudioBz <83524039+ClaudioBz@users.noreply.github.com>
2021-06-03 17:26:11 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,28 +4,29 @@
 
 makeCacheMatrix <- function(x=matrix()){
         inv<-NULL
-        set<-function(y){
+        setted<-function(y){
                 x<<-y
-                inv<<-NULL
+                invers<<-NULL
         }
         get<-function() {x}
-        setInverse<-function(inverse) {inv<<-inverse}
-        getInverse<-function() {inv}
-        list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+        setInverse<-function(inverse) {invers<<-inverse}
+        getInverse<-function() {invers}
+        list(setted=setted,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 ## The function below calculates the mean of the vector "inv". The rule "if("is.null(inv))" checks if the mean have been already calculated.
+## Plus: not sure if inverse of a matrix is the same as the inverse of the values in a matrix.
 
 cacheSolve<-function(x,...){
-        inv<-x$getInverse()
-        if(!is.null(inv)){
-                message("getting cached data")
-                return(inv)
+        invers<-x$getInverse()
+        if(!is.null(invers)){
+                message("loading cached data into function")
+                return(invers)
         }
-        mat<-x$get()
-        inv<-solve(mat,...)
-        x$setInverse(inv)
-        inv
+        mater<-x$get()
+        invers<-solve(mat,...)
+        x$setInverse(invers)
+        invers
 }
         ## Return a matrix that is the inverse of 'x'
 }
